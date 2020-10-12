2a6011ad008409ca47a144810d488d782a82c404
Pascal Thomet <pthomet@gmail.com>
2020-10-12 15:59:09 +0200

    nice

--- count_words_lib.h
+++ count_words_lib.h
@@ -4,6 +4,9 @@
 using namespace std;
 namespace test
 {
+    /**
+     * Class for handling all the string utilities. Only splitLines is avail at the moment.
+     */
     class StringUtilities
     {
     public:
